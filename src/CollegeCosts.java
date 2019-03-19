f361e043e56381df1e2d23d819621e52e8d68be1
eimeartwalsh <eimeartwalsh@gmail.com>
2019-03-19 23:51:55 +0000

    Stubbed out code for CollegeCost problem

---
+++ src/CollegeCosts.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+public class CollegeCosts {
+
+    public static String getName() {
+        // TODO
+    }
+
+    public static String getYear() {
+        // TODO
+    }
+
+    public static void totalCosts(String name) {
+        // TODO
+    }
+
+    public static boolean onCampus() {
+        // TODO
+    }
+
+
+    public static double livingExpenses() {
+        // TODO
+    }
+
+    public static double academicSupplies() {
+        // TODO
+    }
+
+    public static int creditHours() {
+        // TODO
+    }
+
+    public static double creditCosts() {
+        // TODO
+    }
+
+    public static double calculateCosts() {
+        // TODO
+    }
+
+}
